6813a7a7b4302320a4cca31ac74a429b8e489053
noam <noam60@gmail.com>
2021-11-11 23:39:42 +0200

    updated.

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main()
     printf("%d\n", add(-1799608842, -1505362750));
     printf("%d\n", add(-1193932268, -739412326));
     printf("%d\n", add(-8, -8));
-    printf("%d\n", add(-8, 9));
+    printf("%d\n", add(-8, 9));  // finished printing calculations of add!
     printf("%d\n", sub(-1, 4));
     printf("%d\n", sub(3, 5));
     printf("%d\n", sub(-9, -7));
@@ -51,7 +51,7 @@ int main()
     printf("%d\n", sub(-1193932268, 739412326));
     printf("%d\n", sub(1690446029, -1227832313));
     printf("%d\n", sub(-1485781014, 1941883332));
-    printf("%d\n", sub(1021042896, -1734076753));
+    printf("%d\n", sub(1021042896, -1734076753));  // finished printing calculations of sub!
     printf("%d\n", multi(10, 5));
     printf("%d\n", multi(1, 8));
     printf("%d\n", multi(-9, -7));
@@ -75,7 +75,7 @@ int main()
     printf("%d\n", multi(2147483647, 2147483647));
     printf("%d\n", multi(5, -3));
     printf("%d\n", multi(-1799608842, -1505362750));
-    printf("%d\n", multi(-1193932268, -739412326));
+    printf("%d\n", multi(-1193932268, -739412326)); // finished printing calculations of multi!
     printf("%d\n", equal(4, 4));
     printf("%d\n", equal(0, -0));
     printf("%d\n", equal(1374905205, 1374905205));
@@ -88,7 +88,7 @@ int main()
     printf("%d\n", equal(4, 4));
     printf("%d\n", equal(0, -0));
     printf("%d\n", equal(1374905205, 1374905205));
-    printf("%d\n", equal(-2147483648, 0)); // finished tested for equal!
+    printf("%d\n", equal(-2147483648, 0)); // finished printing calculations of equal!
     printf("%d\n", greater(4, 4));
     printf("%d\n", greater(0, -2147483648));
     printf("%d\n", greater(0, -0));
@@ -102,5 +102,5 @@ int main()
     printf("%d\n", greater(2147483647, -2147483648));
     printf("%d\n", greater(1374905205, 1374905205));
     printf("%d\n", greater(-2147483648, 0));
-    printf("%d\n", greater(4, 4));
+    printf("%d\n", greater(4, 4)); // finished printing calculations of greater!
 }
